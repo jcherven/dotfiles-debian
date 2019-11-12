b4bda01dc6925c9cbb32467810e3289773ec30ce
jcherven <jcherven@mail.usf.edu>
2019-11-12 13:24:33 -0500

    Changes viminit.sh to use the new folder names

--- initscripts/viminit.sh
+++ initscripts/viminit.sh
@@ -5,14 +5,14 @@ set -e
 if [ "$(command -v nvim)" ]
 then
   curl -fLo "$HOME/.config/nvim/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-  ln -s "$HOME/dotfiles-mac/config/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+  ln -s "$HOME/dotfiles/config/nvim/init.vim" "$HOME/.config/nvim/init.vim"
   nvim --headless +PlugInstall +qa
 fi
 
 if [ "$(command -v vim)" ]
 then
   curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-  ln -s "$HOME/dotfiles-mac/config/nvim/init.vim" "$HOME/.vimrc"
+  ln -s "$HOME/dotfiles/config/nvim/init.vim" "$HOME/.vimrc"
   vim -c 'PlugInstall | qa'
 fi
 
