7ce89fd7f0961cb0e908228ef32dc73ceb5fe035
jcherven <jcherven@mail.usf.edu>
2019-12-05 12:48:08 -0500

    dotfilesinit.sh: cleans up for easier maintentance

--- dotfilesInit.sh
+++ dotfilesInit.sh
@@ -1,51 +1,69 @@
 #!/bin/bash
 
 set -e
-pushd "$HOME"
 
-# Install the OS's basic environment bootstrap apps (currently supports MacOS and Debian)
-source "$HOME/dotfiles/initscripts/swbootstrap.sh"
+# Some setup of directories that will be used by dotfiles
+# Listing of directories needed by dotfiles. Add new needed entries to this array.
+DIRECTORIES=(
+  # directories needed in ~/.config
+  "$HOME/.config/nvim"
+  "$HOME/.config/ranger"
+  "$HOME/.config/git"
+  )
 
-# Symlinks for files that are located in ~/
-# the arrays FILESLOCAL and FILESLINKED correspond by index order
-FILESLOCAL=(
+# Create directories defined in DIRECTORIES array. Requires /bin/bash for C-style looping to access array indexes {{{
+for ((j=0; j<${#DIRSLOCAL[@]}; ++j)); do
+  if [ ! -d "${DIRSLOCAL[$j]}" ]; then
+    mkdir -p "${DIRSLOCAL[$j]}"
+  fi
+done
+#}}}
+
+# Symlinked files
+# Listing of symlinks for files that are located in ~/
+FILESLOCAL=( # items correspond  to FILESLINKED by index order
+  # Xorg configs
   "$HOME/.Xresources"
+  # bash and other shell configs
   "$HOME/.bash_profile"
   "$HOME/.bashrc"
   "$HOME/.inputrc"
   "$HOME/.zshrc"
+  # tmux
   "$HOME/.tmux.conf"
-  "$HOME/.config/ranger/rc.conf"
-  "$HOME/.config/ranger/rifle.conf"
+  # git global configs
   "$HOME/.config/git/config"
   "$HOME/.config/git/ignore"
   "$HOME/.config/git/git-completion.bash"
   "$HOME/.config/git/git-prompt.sh"
+  # other applications
+  "$HOME/.config/ranger/rc.conf"
+  "$HOME/.config/ranger/rifle.conf"
 )
 
 FILESLINKED=(
+  # Xorg configs
   "$HOME/dotfiles/Xresources"
+  # bash and other shell configs
   "$HOME/dotfiles/bash_profile"
   "$HOME/dotfiles/bashrc"
   "$HOME/dotfiles/inputrc"
   "$HOME/dotfiles/zshrc"
+  # tmux
   "$HOME/dotfiles/tmux.conf"
-  "$HOME/dotfiles/config/ranger/rc.conf"
-  "$HOME/dotfiles/config/ranger/rifle.conf"
+  # git global configs
   "$HOME/dotfiles/config/git/config"
   "$HOME/dotfiles/config/git/ignore"
   "$HOME/dotfiles/config/git/git-completion.bash"
   "$HOME/dotfiles/config/git/git-prompt.sh"
+  # other applications
+  "$HOME/dotfiles/config/ranger/rc.conf"
+  "$HOME/dotfiles/config/ranger/rifle.conf"
 )
-# Symlinks for directories that are located ~/
-# the arrays DIRSLOCAL and DIRSLINKED correspond by index order
-# DIRSLOCAL=(
-#   "$HOME/.config/git"
-# )
 
-# DIRSLINKED=(
-#   "$HOME/dotfiles/config/git"
-# )
+# Call software bootstrap install/config scripts {{{
+# Install the OS's basic environment bootstrap apps (currently supports MacOS)
+source "$HOME/dotfiles/initscripts/swbootstrap.sh"
 
 # Install vim-plug and bootstrap the vim/neovim environment
 source "$HOME/dotfiles/initscripts/viminit.sh"
@@ -54,18 +72,9 @@ source "$HOME/dotfiles/initscripts/viminit.sh"
 # if [[ "$OSTYPE" == "darwin"* ]]; then
 #   source "$HOME/dotfiles/initscripts/macosinit.sh"
 # fi
+#}}}
 
-# create the ranger directory in .config
-if [ ! -d "$HOME/.config/ranger" ]; then
-  mkdir -p "$HOME/.config/ranger"
-fi
-
-# create the git directory in .config
-if [ ! -d "$HOME/.config/git" ]; then
-  mkdir -p "$HOME/.config/git"
-fi
-
-# ~/ dot file symlinks
+# Set the ~/ dot file symlinks defined in FILESLINKED {{{
 # requires a c-style loop to access array index numbers
 for ((i=0; i<${#FILESLOCAL[@]}; ++i)); do
   if [ -e "${FILESLOCAL[$i]}" ];
@@ -74,21 +83,8 @@ for ((i=0; i<${#FILESLOCAL[@]}; ++i)); do
   fi
   ln -s "${FILESLINKED[$i]}" "${FILESLOCAL[$i]}"
 done
-
-# ~/.config directory symlinks
-# for ((j=0; j<${#DIRSLOCAL[@]}; ++j)); do
-#   if [ ! -d "${DIRSLOCAL[$j]}" ]
-#   then
-#     mkdir "${DIRSLOCAL[$j]}"
-#   fi
-#   if [ -d "${DIRSLOCAL[$j]}" ]
-#   then
-#     mv "${DIRSLOCAL[$j]}" "${DIRSLOCAL[$j]}.old"
-#   fi
-#   ln -s "${DIRSLINKED[$j]}" "${DIRSLOCAL[$j]}"
-# done
-
-popd
+#}}}
 
 echo "dotfileInit.sh has completed."
 
+# ex: set foldmethod=marker
